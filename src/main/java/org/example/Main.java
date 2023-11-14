d8514adec4ac033c155e8ff16877556ffe361cb4
borussia444423 <jung78978978@naver.com>
2023-11-14 11:34:25 +0900

    문자 뒤집기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,55 +1,29 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    /** 문자 찾기
-     public int solution(String str, char t) {
-     int answer = 0;
-     str = str.toUpperCase();
-     t = Character.toUpperCase(t);
-     for (char x : str.toCharArray()) {
-     if (x == t) {
-     answer++;
-     }
-     }
-     return answer;
-     } */
-
-    /**
-     * 대소문자 변환하기1
-    public String solution(String str) {
-        String answer = "";
-        for (char x : str.toCharArray()) {
-            if (Character.isLowerCase(x)) {
-                answer += Character.toUpperCase(x);
-            } else {
-                answer += Character.toLowerCase(x);
-            }
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (String x : str) {
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
         }
-        return answer;
-    } */
 
-    /**
-     * 대소문자 변환하기2
-     */
-    public String solution2(String str) {
-        String answer = "";
-        for (char x : str.toCharArray()) {
-            if (x >= 97 && x <= 122) {
-                answer += (char) (x - 32);
-            } else {
-                answer += (char) (x + 32);
-            }
-        }
         return answer;
     }
-
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        String str = kb.next();
-        System.out.println(T.solution2(str));
+        int n = kb.nextInt();
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = kb.next();
+        }
+        for (String x : T.solution(n, str)) {
+            System.out.println(x);
+        }
     }
 }
