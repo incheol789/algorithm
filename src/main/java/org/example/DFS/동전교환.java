397bbf74e8ee86b856c8e5b8834b730c8f9c4f28
borussia444423 <jung78978978@naver.com>
2024-01-10 07:38:38 +0900

    commit

--- src/main/java/org/example/DFS/동전교환.java
+++ src/main/java/org/example/DFS/동전교환.java
@@ -1,15 +1,18 @@
 package org.example.DFS;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class 동전교환 {
 	static int n, m, answer = Integer.MAX_VALUE;
-
-	public void DFS(int L, int sum, int[] arr) {
+	public void DFS(int L, int sum, Integer[] arr) {
 		if (sum > m) return;
+		if (L >= answer) return;
 		if (sum == m) {
 			answer = Math.min(answer, L);
-		} else {
+		}
+		else {
 			for (int i = 0; i < n; i++) {
 				DFS(L + 1, sum + arr[i], arr);
 			}
@@ -20,10 +23,11 @@ public class 동전교환 {
 		동전교환 T = new 동전교환();
 		Scanner kb = new Scanner(System.in);
 		n = kb.nextInt();
-		int[] arr = new int[n];
+		Integer[] arr = new Integer[n];
 		for (int i = 0; i < n; i++) {
 			arr[i] = kb.nextInt();
 		}
+		Arrays.sort(arr, Collections.reverseOrder());
 		m = kb.nextInt();
 		T.DFS(0, 0, arr);
 		System.out.println(answer);
--- src/main/java/org/example/DFS/중복순열.java
+++ src/main/java/org/example/DFS/중복순열.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class 중복순열 {
 	static int[] pm;
 	static int n, m;
+
 	public void DFS(int L) {
 		if (L == m) {
 			for (int x : pm) System.out.print(x + " ");
-				System.out.println();
+			System.out.println();
 		} else {
 			for (int i = 1; i <= n; i++) {
 				pm[L] = i;
