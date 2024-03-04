501052d49bf890857153f4f48dedc0ae9a34cb2f
borussia444423 <jung78978978@naver.com>
2024-03-05 08:54:28 +0900

    백준 2606 DFS

---
+++ src/main/java/org/example/DFS_2/백준_2606_바이러스.java
@@ -0,0 +1,52 @@
+package org.example.DFS_2;
+
+import java.util.*;
+import java.io.*;
+
+public class 백준_2606_바이러스 {
+	static boolean[][] graph;
+	static boolean[] visited;
+	static int N, M;
+	static int answer;
+
+	public static void DFS(int idx) {
+		visited[idx] = true;
+		answer++;
+
+		for (int i = 1; i <= N; i++) {
+			if (visited[i] == false && graph[idx][i])
+				DFS(i);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		// 0. 입력 및 초기화
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+
+		graph = new boolean[N + 1][N + 1];
+		visited = new boolean[N + 1];
+
+		// 1. graph에 연결 정보 채우기
+		int x, y;
+		for (int i = 0; i < M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			x = Integer.parseInt(st.nextToken());
+			y = Integer.parseInt(st.nextToken());
+			graph[x][y] = true;
+			graph[y][x] = true;
+		}
+
+		// 2. DFS(재귀함수) 호출
+		DFS(1);
+
+		// 3. 출력
+		bw.write(String.valueOf(answer - 1));
+
+		br.close();
+		bw.close();
+	}
+}
